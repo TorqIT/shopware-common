c073b96fa785fa58f511f3bc6771ab82ed48332e
Andrew King <andrew.king@torqit.ca>
2024-12-24 13:25:15 -0400

    updates to import and export command.  Need to do some more testing and create documentation.

---
+++ src/Command/EntityExportCommand.php
@@ -0,0 +1,163 @@
+<?php declare(strict_types=1);
+
+namespace Torq\Shopware\Common\Command;
+
+use Throwable;
+use Shopware\Core\Framework\Context;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\WriteProtected;
+use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
+
+class EntityExportCommand extends Command
+{
+    public function __construct(private readonly DefinitionInstanceRegistry $definitionRegistry)
+    {
+        parent::__construct(); 
+    }
+   
+    public function configure()
+    {
+        $this->setName('torq:entity-exporter')
+            ->setDescription('Export entities');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $configFile = '/var/www/html/custom/data/_config.json';        
+        try{
+            $config = json_decode(file_get_contents($configFile),true);            
+        }catch(Throwable $e){
+            $io->getErrorStyle()->warning("Error loading config file",true);
+            return Command::FAILURE;
+        }            
+
+        foreach($config as $entityConfig){
+            $entity = $entityConfig["entity"];
+            $ids = array_key_exists("ids",$entityConfig) ? $entityConfig["ids"] : [];
+            $associations = array_key_exists("associations",$entityConfig) ? $entityConfig["associations"] : [];
+            $extraCriteria = array_key_exists("criteria",$entityConfig) ? $entityConfig["criteria"] : [];
+            $repo = $this->definitionRegistry->getRepository($entity);
+
+            //if ids are set, add to the criteria plus add any associations and extra search criteria
+            $criteria = empty($ids) ? new Criteria() : new Criteria($ids);
+            $criteria = $this->addAssociations($criteria, $associations);    
+            $criteria = $this->addCriteria($criteria, $extraCriteria);
+
+            $elements = $repo->search($criteria, Context::createDefaultContext())->getEntities()->getElements();  
+
+            $jsonOutput = $this->processUnwritableFields($repo,$elements,$associations);
+
+            $file = '/var/www/html/custom/data/' . $entity . '.json';
+            // Write JSON data to the file, overwriting if it already exists
+            file_put_contents($file, $jsonOutput);
+
+            $io->write("Success exporting entity - " . $entity, true);
+        }
+        return Command::SUCCESS;
+    }
+
+    private function addAssociations($criteria, $associations){
+        foreach($associations as $association){
+            $criteria->addAssociation($association);
+        }
+    }
+
+    private function addCriteria($criteria, $extraCriteria){
+        foreach($extraCriteria as $crit){
+            $type = $crit["type"];
+            $field = $crit["field"];
+            $values = $crit["values"]; //array
+            switch($type){
+                case 'EqualsAny':
+                    $criteria->addFilter(new EqualsAnyFilter($field,$values));
+                    break;
+            }
+        }
+        return $criteria;
+    }
+
+    private function processUnwritableFields($repo, $elements, $associations) {
+
+        $json = json_decode(json_encode($elements, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_INVALID_UTF8_SUBSTITUTE | JSON_PARTIAL_OUTPUT_ON_ERROR),true);
+
+        $fields = $repo->getDefinition()->getFields();            
+        //Process unwriteable fields at the top level
+        $unwritableFields = [];
+        foreach($fields as $field){
+            if($field->getFlag(WriteProtected::class) || $field instanceof ManyToOneAssociationField){
+                $unwritableFields[] = $field->getPropertyName();
+            }
+        }
+        //Process unwriteable fields for the associations
+        foreach($associations as $association){
+            $entityAssoc = explode(".",$association);
+            $entityDef = $repo->getDefinition(); // start with the main def
+            $breadCrumb = '';
+            foreach($entityAssoc as $a){
+                $breadCrumb = (empty($breadCrumb) ? $a : $breadCrumb . '.' . $a);
+                $entityElements = $entityDef->getFields()->getElements();
+                $entityElementField = $entityElements[$a];
+                if($entityElementField instanceof OneToManyAssociationField){
+                    $entityAssocDef = $entityElementField->getReferenceDefinition();
+                    $entityFields = $entityAssocDef->getFields();
+                    foreach($entityFields as $entityField){
+                        if($entityField->getFlag(WriteProtected::class) || $entityField instanceof ManyToOneAssociationField){
+                            $unwritableFields[] = $breadCrumb . '.' . $entityField->getPropertyName();
+                        }
+                    }
+                    $entityDef = $entityAssocDef; // set up for the next nested association
+                }
+            }
+        }
+
+        $arrayElements = [];          
+        foreach($json as $element){
+            foreach($unwritableFields as $value){
+                $values = explode(".",$value);//children removals will have at least one "." in the value
+                if(count($values) == 1){
+                    unset($element[$value]);
+                }else{
+                    $element = $this->removeKeyFromArray($values, $element);
+                }
+            }
+            $arrayElements[] = $element;
+        }        
+
+        return json_encode($arrayElements, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_INVALID_UTF8_SUBSTITUTE | JSON_PARTIAL_OUTPUT_ON_ERROR);
+    }
+
+    /**
+     * Recursive function to remove unwriteable fields from the json.
+     * 
+     * @param array $values 
+     * @param array $data 
+     * @return array 
+     */
+    private function removeKeyFromArray(array $keys, array $data): array {        
+        $firstKey = array_shift($keys); //pop off the first value
+
+        //if any values left keep going deeper
+        if(count($keys) > 0){     
+            $nextData = $data[$firstKey];
+            for($i=0; $i<count($nextData);$i++){
+                $data[$firstKey][$i] = $this->removeKeyFromArray($keys, $nextData[$i]);
+            }
+        }else{
+            //only do an unset if there was only one key so we don't remove the parents
+            unset($data[$firstKey]);
+        }
+
+        return $data;
+     }
+  }
---
+++ src/Command/EntityImportCommand.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace Torq\Shopware\Common\Command;
+
+use Exception;
+use Throwable;
+use Shopware\Core\Framework\Context;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\WriteProtected;
+use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+
+class EntityImportCommand extends Command
+{
+    public function __construct(private readonly DefinitionInstanceRegistry $definitionRegistry)
+    {
+        parent::__construct(); 
+    }
+   
+    public function configure()
+    {
+        $this->setName('torq:entity-importer')
+            ->setDescription('Import entities');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $configFile = '/var/www/html/custom/data/_config.json';        
+        try{
+            $config = json_decode(file_get_contents($configFile),true);            
+        }catch(Throwable $e){
+            $io->getErrorStyle()->warning("Error loading config file",true);
+            return Command::FAILURE;
+        }   
+        
+        foreach($config as $entityConfig){
+            $entity = $entityConfig["entity"];
+            $repo = $this->definitionRegistry->getRepository($entity);
+            
+            $file = '/var/www/html/custom/data/' . $entity . '.json';        
+            try{
+                $data = json_decode(file_get_contents($file),true);
+                try{
+                    $repo->upsert($data,Context::createDefaultContext());
+                    $io->write("Success importing entity - " . $entity, true);
+                }catch(Throwable $e){
+                    //svar_dump($data);
+                    print_r($data);
+                    throw $e;
+                }
+            }catch(Throwable $e){
+                $io->getErrorStyle()->warning("Error importing entity - " . $entity . ".\n" . $e->getMessage(),true);
+            }            
+        }
+        return Command::SUCCESS;
+    }
+
+  }
