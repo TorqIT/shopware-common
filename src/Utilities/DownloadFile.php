3d264abfadf7040dd7bf4286771d80a62b643bc0
Andrew King <andrew.king@torqit.ca>
2025-01-31 19:09:15 -0400

    Add a donwloadAsPdf function

--- src/Utilities/DownloadFile.php
+++ src/Utilities/DownloadFile.php
@@ -2,6 +2,9 @@
 
 namespace Torq\Shopware\Common\Utilities;
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Response;
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
@@ -28,6 +31,30 @@ class DownloadFile
 
         unlink($tmpXLSX);
 
+        return $response;
+    }
+
+    public static function downloadAsPdf($response, $domRef = null, $filename = 'download'): Response {
+        if($domRef == null)
+            $html = $response->getContent();
+        else
+            $html = (new Crawler($response->getContent()))->filter($domRef)->first()->html();
+        
+        $options = new Options();
+        $options->set('isRemoteEnabled', true);                      
+        
+        $dompdf = new Dompdf($options);
+        //$a4 = [0, 0, 595.28, 841.89];
+        //$dompdf->setPaper($a4, 'portrait');
+        
+        $dompdf->loadHtml($html);
+        $dompdf->render();            
+
+        $content = $dompdf->output();
+        $contentType = 'application/pdf';
+        $response = new Response($content ?: null, 200, ['Content-Type' => $contentType,
+                                                        'Content-Disposition' => 'filename='. $filename . '.pdf']);
+        $response->setLastModified((new \DateTimeImmutable()));
         return $response;
     }
 }
