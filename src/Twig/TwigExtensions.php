a58ef116169837a3f82d4a74d3c233d6ff88d0d8
Andrew King <andrew.king@torqit.ca>
2025-04-10 10:34:17 -0300

    Email and template renderer pluf twig extension

---
+++ src/Twig/TwigExtensions.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Torq\Shopware\Common\Twig;
+
+use Twig\TwigFunction;
+use Twig\Extension\AbstractExtension;
+
+class TwigExtensions extends AbstractExtension
+{
+    public function __construct()
+    {
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('json_decode',         [$this, 'jsonDecode'])
+        ];
+    }
+
+    /**
+     * Uses PHP json_decode to create a an array from a json string
+     * 
+     * @param string $str 
+     * @return mixed 
+     */
+    public function jsonDecode(string $str){
+        $arr = json_decode($str,true);
+        return $arr;
+    }
+}
