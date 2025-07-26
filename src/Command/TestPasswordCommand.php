0db74ef633f63a164468eba65bb6e222d2a87207
Jeremy Noonan <jeremy.noonan@torqit.ca>
2025-07-26 13:00:26 -0300

    adding regex support to password

--- src/Command/TestPasswordCommand.php
+++ src/Command/TestPasswordCommand.php
@@ -6,11 +6,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Torq\Shopware\Common\Security\Encryption\EncryptionHandler;
 use Symfony\Component\Validator\Constraints\PasswordStrengthValidator;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Torq\Shopware\Common\Constants\ConfigConstants;
 
 class TestPasswordCommand extends Command
 {
+    public function __construct(private SystemConfigService $systemConfigService)
+    {
+        parent::__construct();
+    }
     
     protected function configure(): void
     {
@@ -26,11 +31,48 @@ class TestPasswordCommand extends Command
 
         $b = PasswordStrengthValidator::estimateStrength($password);
 
-        $output->writeln([
-            'Password: ' . $password,
-            'Length: ' . strlen($password),
-            'Strength: ' . $b,
-        ]);
+        $configuredPasswordStrength = $this->systemConfigService->getInt(ConfigConstants::PASSWORD_STRENGTH);
+
+        if($configuredPasswordStrength === ConfigConstants::PASSWORD_STRENGTH_REGEX){
+            $output->writeln('Password validation is configured to use regex pattern');
+            
+            $regex = $this->systemConfigService->getString(ConfigConstants::PASSWORD_REGEX);
+            $output->writeln('Testing against pattern: ' . $regex . ' with password: ' . $password);
+            
+            $table = new \Symfony\Component\Console\Helper\Table($output);
+            $table->setHeaders(['Test', 'Result']);
+            
+            $matches = preg_match($regex, $password);
+            $table->addRow(['Regex Match', $matches ? '<info>PASS</info>' : '<error>FAIL</error>']);
+            
+            $table->render();
+        }
+        else{
+            $strengthLabels = [
+                1 => 'Weak',
+                2 => 'Medium', 
+                3 => 'Strong',
+                4 => 'Very Strong'
+            ];
+            
+            $output->writeln('Required password strength: ' . $strengthLabels[$configuredPasswordStrength]);
+            
+            $table = new \Symfony\Component\Console\Helper\Table($output);
+            $table->setHeaders(['Test', 'Result']);
+            
+            $strengthLabel = match(true) {
+                $b >= 4 => 'Very Strong',
+                $b >= 3 => 'Strong',
+                $b >= 2 => 'Medium',
+                default => 'Weak'
+            };
+            
+            $table->addRow(['Password Strength', $strengthLabel]);
+            $table->addRow(['Meets Configuration', $b >= $configuredPasswordStrength ? '<info>PASS</info>' : '<error>FAIL</error>']);
+            
+            $table->render();
+        }
+       
         
         return self::SUCCESS; 
     }
---
+++ src/Constants/ConfigConstants.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Torq\Shopware\Common\Constants;
+
+class ConfigConstants
+{
+    public const CATEGORY_FILTER = 'TorqShopwareCommon.config.categoryFilter';
+    public const CATEGORY_FILTER_MODE = 'TorqShopwareCommon.config.categoryFilterMode';
+    public const RESTRICT_PROPERTIES_ON_LISTING = 'TorqShopwareCommon.config.restrictPropertiesOnListing';
+    public const INST_STOCK_FILTER = 'TorqShopwareCommon.config.instockFilter';
+    public const PASSWORD_STRENGTH = 'TorqShopwareCommon.config.passwordStrength';
+    public const PASSWORD_REGEX = 'TorqShopwareCommon.config.passwordRegex';
+    public const PASSWORD_STRENGTH_REGEX = -1;
+    public const RESTRICT_ADMIN_BY_IP = 'TorqShopwareCommon.config.restrictAdminByIP';
+    public const RESTRICT_ADMIN_IP_LIST = 'TorqShopwareCommon.config.restrictAdminIPList';
+    public const QUICK_ADD_AUTOCOMPLETE_ENABLED = 'TorqShopwareCommon.config.quickAddAutocompleteEnabled';
+    public const QUICK_ADD_STACKABLE_ENABLED = 'TorqShopwareCommon.config.quickAddStackableEnabled';
+    public const QUICK_ADD_ADVANCED_SEARCH_ENABLED = 'TorqShopwareCommon.config.quickAddAdvancedSearchEnabled';
+}
